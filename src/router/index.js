8834410b9ab71097ce3781b352c0d5f76183d965
qabrah <qabrah@gmail.com>
2022-12-02 18:40:21 -0600

    FEAT: Configuracion para uso de vuetify y vue router

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../components/HelloWorld.vue'
+import AboutView from '../components/About.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
