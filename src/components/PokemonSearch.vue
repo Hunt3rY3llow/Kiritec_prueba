f7f4055a1fce746fe19f5841becff00a91bd6f72
qabrah <qabrah@gmail.com>
2022-12-04 17:34:32 -0600

    FEAT: vistas del sistema

---
+++ src/components/PokemonSearch.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="searchbar">
+    <form @submit.prevent="setPokemonUrl">
+      <input type="text" placeholder="Busqueda de pokemon por nombre o numero de pokedex" v-model="searchvalue">
+      <p class="leyenda">Ingresa nombre o numero del pokemon en letras minusculas</p>
+    </form>
+    <i class="fas fa-search" @click="setPokemonUrl"></i>
+  </div>
+</template>
+
+<script>
+
+ /* eslint-disable */
+  export default {
+    props: [
+      'apiUrl'
+    ],
+    data: () => {
+      return {
+        searchvalue: '',
+      }
+    },
+    methods: {
+      setPokemonUrl(url) {
+        if(this.searchvalue !== '')
+          this.$emit('setPokemonUrl', this.apiUrl + this.searchvalue);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .searchbar {
+        position: relative;
+        width: 100%;
+        max-width: 510px;
+        padding-bottom: 20px;
+    }
+    input {
+      border: none;
+      outline: none;
+      border-radius: 5px;
+      padding: 10px 40px 10px 10px;
+      width: calc(100% - 50px);
+      font-size: 1rem;
+      box-shadow: 0 15px 30px rgba(0,0,0,.2),
+                  0 10px 10px rgba(0,0,0,.2);
+    }
+    .leyenda {
+        color: #fff;
+    }
+    i {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      font-size: 1.25rem;
+      color: #0A2E50;
+      cursor: pointer;
+    }
+</style>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,60 @@
+/* eslint-disable */
+<template>
+   <v-app >
+      <v-main>
+         <v-container>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm12 md12>
+                  <v-card min-width=970px class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Formulario de Registro</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                     
+                            <v-text-field
+                              v-model="email"
+                              name="email"
+                              label="Email Usuario"
+                              type="text"
+                              placeholder="Correo usuario"
+                              required
+                           ></v-text-field>
+                           
+                            <v-text-field
+                              v-model="password"
+                              name="password"
+                              label="Enter password"
+                              type="password"
+                               placeholder="Contraseña"
+                               required
+                           ></v-text-field>
+
+                           <v-btn class="mt-4" color="primary" @click="register">Crear cuenta</v-btn>
+                     
+                     </v-card-text>
+                  </v-card>
+                
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-main>
+   </v-app>
+</template>
+
+<script setup>
+ /* eslint-disable */
+  import {ref} from "vue";
+  import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+  import {useRouter} from 'vue-router';
+  const email=ref("");
+  const password=ref("");
+  const router = useRouter();
+  const register =() =>{
+   createUserWithEmailAndPassword(getAuth(),email.value,password.value)
+   .then((data)=>{
+    console.log("Usuario registrado");
+    router.push('/');
+   })
+  }
+</script>
+
